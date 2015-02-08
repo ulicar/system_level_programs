453d19b29fc28ce98050a998338d05a6c68b3fd8
Josip Domsic <josip.domsic@gmail.com>
2015-02-08 22:31:37 +0100

    changed function names to english, refactor v3, added find_array_index

--- interrupt_generator_usage/interrupt_handler.c
+++ interrupt_generator_usage/interrupt_handler.c
@@ -2,94 +2,54 @@
 #include <signal.h>
 #include "signals.h"
 
-#define N SIGNALS+1
 
-int OZNAKA_CEKANJA[N];
-int PRIORITET[N];
-int TEKUCI_PRIORITET;
+int WAITING_LIST[SIGNALS];
+int PRIORITY[SIGNALS];
+int CURRENT_PRIORITY;
 
 void disable_interrupts (void) {
    for(int i=0; i < SIGNALS; i++)
-      sighold ( sig[i] );
+      sighold (SIGNAL_TYPES[i]);
    }
 
 void enable_interrupts (void) {
    for(int i=0; i < SIGNALS; i++)
-      sigrelse(sig[i]);
+      sigrelse (SIGNAL_TYPES[i]);
    }
 
-void obrada_prekida(int i){
-       int j;
-      switch(i){
-      case 1:for(j=1;j<6;j++){
-             printf("- %d - - - -\n",j);
-             sleep(1);}
-           break;
-      case 2:for(j=1;j<6;j++){
-             printf("- - %d - - -\n",j);
-             sleep(1);}
-           break;
-      case 3:for(j=1;j<6;j++){
-             printf("- - - %d - -\n",j);
-             sleep(1);}
-           break;
-      case 4:for(j=1;j<6;j++){
-             printf("- - - - %d -\n",j);
-             sleep(1);}
-           break;
-      case 5:for(j=1;j<6;j++){
-             printf("- - - - - %d\n",j);
-             sleep(1);}
-           break;
-      }
+void simmulate_interrupt_routine(int interrupt){
+
+  for(int job_stage = 0; job_stage <= 5; job_stage++) print_job_stage(job_stage);
+      for(int iter = 0; iter < interrupt; iter++) printf ("- ");
+      printf("%d ", stage);
+      for(int iter = SIGNALS; iter > interrupt; iter--) printf ("- ");
+      sleep (1);
+      
   }
 
 void interrupt_handler(int sig){
   int n,x,k;
-  zabrani_prekidanje();
-   switch(sig){
-      case SIGUSR1:
-           n=1;
-           printf("- X - - - -\n");
-           break;
-      case SIGUSR2:
-           n=2;
-           printf("- - X - - -\n");
-           break;
-      case SIGTERM:
-           n=3;
-           printf("- - - X - -\n");
-           break;
-      case SIGQUIT:
-           n=4;
-           printf("- - - - X -\n");
-           break;
-      case SIGINT:
-           n=5;
-           printf("- - - - - X\n");
-           break;
-      }
+  disable_interrupts();
+  print_recieved_interrupt(sig);
 
-  OZNAKA_CEKANJA[n]++;
+  WAITING_LIST[n]++;
   do{
-       x=0;
-       for(k=1;k<6;k++)
-           if((OZNAKA_CEKANJA[k]!=0) && (k>TEKUCI_PRIORITET))
-                x=k;
-  if (x>0){
-           OZNAKA_CEKANJA[x]--;
-           PRIORITET[x] = TEKUCI_PRIORITET;
-           TEKUCI_PRIORITET= x;
-           dozvoli_prekidanje();
-           obrada_prekida(x);
-           zabrani_prekidanje();
-          
-           }
-  }while(x>0);
-  TEKUCI_PRIORITET = PRIORITET[x];
-           dozvoli_prekidanje();
-
-
+    x=0;
+    for(k=1;k<6;k++)
+       if((WAITING_LIST[k]!=0) && (k>CURRENT_PRIORITY))
+            x=k;
+    if (x>0){
+      WAITING_LIST[x]--;
+      PRIORITY[x] = CURRENT_PRIORITY;
+      CURRENT_PRIORITY = x;
+      enable_interrupts();
+      simmulate_interrupt_routine(x);
+      disable_interrupts();
+    }
+  } while ( x > 0);
+  
+  CURRENT_PRIORITY = PRIORITY[x];
+  enable_interrupts();
 }
 
 void register_interrupts (void) { 
@@ -104,6 +64,26 @@ void simulate_main_program_execution (void) {
   printf("\n");
   
   sleep(1);
+}
+void print_recieved_interrupt(int interrupt_type) {
+
+  int index = find_array_index (interrupt_type, SIGNAL_TYPES, SIGNALS);
+  if (index != -1) {
+    for(int i = 0; i <= index; i++) printf("- ");
+    printf("X ");
+    for(int i = SIGNALS; i > index; i--) printf("- ");
+  }
+
+}
+
+int find_array_index (int value, int a[], int num_elements) {
+  for (int i=0; i<num_elements; i++) {
+    if (a[i] == value) {
+      return(value);
+    }
+  }
+  
+  return(-1);
 }
 
 void print_main_info (void) {
